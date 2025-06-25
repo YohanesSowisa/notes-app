fc8504f8616418cae53d422e3a840d7704a4dd0e
Yohanes Sowisa <yohanes.sowisa29@gmail.com>
2025-06-25 15:48:51 +0700

    Register, login, and role functions

--- app/db.py
+++ app/db.py
@@ -1,97 +1,185 @@
 import psycopg2
 
 def get_connection():
-  return psycopg2.connect(
-    host="localhost",
-    port=5432,
-    database="postgres",    
-    user="postgres",
-    password="admin" 
-  )
-
-def create_table():
-  print("run")
-  conn = get_connection()
-  cur = conn.cursor() 
-  cur.execute("""
-    CREATE TABLE IF NOT EXISTS notes (
-        id SERIAL PRIMARY KEY,
-        note TEXT NOT NULL
+    return psycopg2.connect(
+        host="db",
+        port=5432,
+        database="postgres",
+        user="postgres",
+        password="admin",
     )
-  """)
 
-  conn.commit() # 
-  cur.close()
-  conn.close()
 
-def insert_note(note):
-  conn = get_connection()
-  cur = conn.cursor()
-  cur.execute("INSERT INTO notes (note) VALUES (%s)", (note,))
-  conn.commit()
-  cur.close()
-  conn.close()
+def create_notes_table():
+    conn = get_connection()
+    cur = conn.cursor()
+    cur.execute(
+      """
+      CREATE TABLE IF NOT EXISTS notes (
+        id SERIAL PRIMARY KEY,
+        note TEXT NOT NULL,
+        created_by TEXT NOT NULL,
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+      )
+      """
+    )
 
-def get_all_notes():
-  conn = get_connection()
-  cur = conn.cursor()
-  cur.execute("SELECT * FROM notes")
-  notes = cur.fetchall()
-  cur.close()
-  conn.close()
-  return notes
+    conn.commit()
+    cur.close()
+    conn.close()
 
-def delete_note_by_id(note_id):
-  conn = get_connection()
-  cur = conn.cursor()
-  cur.execute("DELETE FROM notes WHERE id = %s", (note_id,))
-  conn.commit()
-  deleted = cur.rowcount > 0
-  cur.close()
-  conn.close()
-  return deleted
 
-def patch_note_by_id(id, patch):
-  conn = get_connection()
-  cur = conn.cursor()
-  # print(cur.statusmessage)
+def create_users_table():
+    conn = get_connection()
+    cur = conn.cursor()
+    
+    cur.execute("DROP TABLE IF EXISTS users")
 
-  if not len(patch['note']) == 0:
-    if "note" in patch:
-      cur.execute("UPDATE notes SET note = %s WHERE id = %s", (patch["note"], id))
-      conn.commit()
-      if cur.statusmessage == 'UPDATE 0':
-        result = f'Catatan dengan id {id} tidak ada'
-      else:
-        result = f"Isi catatan dengan id {id} diubah menjadi {patch['note']}"
+    cur.execute(
+        """
+    CREATE TABLE IF NOT EXISTS users (
+      id SERIAL PRIMARY KEY,
+      username TEXT UNIQUE NOT NULL,
+      password TEXT NOT NULL,
+      role TEXT DEFAULT 'user'
+    )
+  """
+    )
+
+    conn.commit()
+    cur.close()
+    conn.close()
+
+
+def register_user(username, password, role):
+    conn = get_connection()
+    cur = conn.cursor()
+
+    try:
+        cur.execute(
+            "INSERT INTO users (username, password, role) VALUES (%s, %s, %s)",
+            (username, password, role),
+        )
+        conn.commit()
+        result = True
+    except Exception as e:
+        print("Gagal register:", e)
+        conn.rollback()
+        result = False
+    finally:
+        cur.close()
+        conn.close()
+        return result
+
+def get_user_by_username(username):
+    conn = get_connection()
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM users WHERE username = %s", (username,))
+    user = cur.fetchone()
+    cur.close()
+    conn.close()
+    return user
+
+def insert_note(note, created_by):
+    conn = get_connection()
+    cur = conn.cursor()
+    
+    if note:
+      cur.execute(
+          "INSERT INTO notes (note, created_by) VALUES (%s, %s)", (note, created_by['username'])
+      )
+      
+    conn.commit()
+    cur.close()
+    conn.close()
+
+
+def get_all_notes(current_user):
+    conn = get_connection()
+    cur = conn.cursor()
+    
+    if current_user['role'] == 'admin':
+      cur.execute("SELECT * FROM notes")
     else:
-      result = "Tidak ada data yang diubah"
-  else:
-    result = 'Catatan tidak boleh kosong'
+      cur.execute("SELECT * FROM notes WHERE created_by = %s", (current_user['username'],))
+    
+    notes = cur.fetchall()
+    cur.close()
+    conn.close()
+    return notes
 
-  cur.close()
-  conn.close()
-  return result
+
+def get_all_users():
+    conn = get_connection()
+    cur = conn.cursor()
+    # cur.execute("SELECT * FROM users")
+    cur.execute("SELECT id, username, role FROM users")
+    notes = cur.fetchall()
+    cur.close()
+    conn.close()
+    return notes
 
 
-def patch_note_by_id(id, patch):
+def delete_note_by_id(note_id, current_user):
+    conn = get_connection()
+    cur = conn.cursor()
+    
+    if current_user['role'] == 'admin':
+      cur.execute("DELETE FROM notes WHERE id = %s", (note_id,))
+    else:
+      cur.execute("DELETE FROM notes WHERE id = %s AND created_by = %s", (note_id, current_user['username']))
+    
+    conn.commit()
+    deleted = cur.rowcount > 0
+    cur.close()
+    conn.close()
+    return deleted
+
+
+# def patch_note_by_id(id, patch, username):
+#     conn = get_connection()
+#     cur = conn.cursor()
+
+#     if not len(patch["note"]) == 0:
+#         if "note" in patch:
+#             cur.execute("UPDATE notes SET note = %s WHERE id = %s AND created_by = %s", (patch["note"], id, username))
+#             conn.commit()
+#             if cur.statusmessage == "UPDATE 0":
+#                 result = f"Catatan dengan id {id} tidak ada"
+#             else:
+#                 result = f"Isi catatan dengan id {id} diubah menjadi {patch['note']}"
+#         else:
+#             result = "Tidak ada data yang diubah"
+#     else:
+#         result = "Catatan tidak boleh kosong"
+
+#     cur.close()
+#     conn.close()
+#     return result
+
+
+def patch_note_by_id(id, patch, current_user):
     if "note" not in patch:
         return "Tidak ada data yang diubah"
-    
+
     note = patch["note"]
     if not note.strip():
         return "Catatan tidak boleh kosong"
 
     conn = get_connection()
     cur = conn.cursor()
+
+    if current_user['role'] == 'admin':
+      cur.execute("UPDATE notes SET note = %s WHERE id = %s", (note, id))
+    else:
+      cur.execute("UPDATE notes SET note = %s WHERE id = %s AND created_by = %s", (note, id, current_user['username']))
     
-    cur.execute("UPDATE notes SET note = %s WHERE id = %s", (note, id))
     conn.commit()
 
     result = (
         f"Isi catatan dengan id {id} diubah menjadi {note}"
-        if cur.rowcount > 0 else
-        f"Catatan dengan id {id} tidak ada"
+        if cur.rowcount > 0
+        else f"Catatan dengan id {id} tidak ditemukan atau bukan milik Anda"
     )
 
     cur.close()
--- app/main.py
+++ app/main.py
@@ -1,98 +1,144 @@
-# import requests
-# from flask import Flask, request, jsonify
-# from db import create_table, insert_note, get_all_notes, delete_note_by_id
-
-# app = Flask(__name__)
-
-# count = 1
-# notes = []
-
-# @app.route('/notes', methods=['POST'])
-# def post_notes():
-#   global count, notes
-#   user_input = request.get_json()
-
-#   if not user_input or 'note' not in user_input:
-#     return jsonify({"error": "Field 'note' Wajib Diisi"}, 400)
-  
-#   new_note = {
-#     'id' : count,
-#     'note' : user_input['note']
-#   }
-
-#   notes.append(new_note)
-#   count += 1
-
-#   return jsonify(new_note), 201
-
-
-# @app.route('/notes', methods=['GET'])
-# def get_notes():
-#   return jsonify(notes), 200
-
-
-# @app.route('/notes/<int:id>', methods=['DELETE'])
-# def del_notes(id):
-#   global notes
-
-#   note_to_delete = next((n for n in notes if n['id'] == id), None)
-
-#   if note_to_delete:
-#     notes.remove(note_to_delete)
-#     return jsonify({"message": f"Catatan Dengan ID {id} Berhasil Dihapus"}, 200)
-#   else:
-#     return jsonify({"message": f"Catatan Dengan ID {id} Tidak Ditemukan"}, 404)
-
-
-# if __name__ == '__main__':
-#   app.run(debug=True, host="0.0.0.0",port=5000)
-
-
-import requests
+import jwt
+import datetime
 from flask import Flask, request, jsonify
-from db import create_table, insert_note, get_all_notes, delete_note_by_id, patch_note_by_id
+from functools import wraps
+from db import (
+    create_notes_table,
+    create_users_table,
+    register_user,
+    get_user_by_username,
+    insert_note,
+    get_all_notes,
+    get_all_users,
+    delete_note_by_id,
+    patch_note_by_id
+)
 
 app = Flask(__name__)
 
-create_table()
+create_notes_table()
+create_users_table()
 
-@app.route('/notes', methods=['POST'])
-def post_notes():
-  user_input = request.get_json()
+@app.route("/users", methods=["GET"])
+def get_users():
+    users_data = get_all_users()
+    result = [{"id": id, "username": username, "role": role} for (id, username, role) in users_data]
+    return jsonify(result), 200
 
-  if not user_input or 'note' not in user_input:
-    return jsonify({"error": "Field 'note' Wajib Diisi"}), 400
+
+@app.route("/register", methods=["POST"])
+def register():
+    data = request.get_json()
+    username = data.get('username')
+    password = data.get('password')
+    role = data.get('role')
+    
+    if not username or not password:
+      return jsonify({"message": "Username dan/atau password wajib diisi"}), 400
+    
+    success = register_user(username, password, role = 'user' if not role else role)
+    if success:
+      return jsonify({"message": "Registrasi berhasil"}), 201
+    else:
+      return jsonify({"message": "Username sudah digunakan atau error lain"}), 409
+
+
+SECRET_KEY = 'rahasia_super_aman'
+
+
+@app.route("/login", methods=["POST"])
+def login():
+    data = request.get_json()
+    username = data.get('username')
+    password = data.get('password')
+
+    user = get_user_by_username(username)
+
+    if user and user[2] == password:
+        token = jwt.encode({
+            'username': username,
+            'role': user[3],
+            'exp': datetime.datetime.utcnow() + datetime.timedelta(hours=10)
+        }, SECRET_KEY, algorithm='HS256')
+
+        return jsonify({'token': token}), 200
+
+    return jsonify({'message': 'Username/password salah'}), 401
   
-  note = user_input['note']
-  insert_note(note)
-
-  return jsonify({"message": f"Catatan '{note}' berhasil disimpan"}), 201
-
-
-@app.route('/notes', methods=['GET'])
-def get_notes():
-  notes_data = get_all_notes()
-  result = [{"id": id, "note": note} for (id, note) in notes_data]
-  return jsonify(result), 200
-
-
-@app.route('/notes/<int:id>', methods=['DELETE'])
-def del_notes(id):
-  success = delete_note_by_id(id)
-
-  if success:
-    return jsonify({"message": f"Catatan Dengan ID {id} Berhasil Dihapus"}), 200
-  else:
-    return jsonify({"message": f"Catatan Dengan ID {id} Tidak Ditemukan"}), 4044
   
-  
-@app.route('/notes/<int:id>', methods=['PATCH'])
-def patch_notes(id):
-  patch_note = request.get_json()
+def token_required(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        token = None
 
-  patched = patch_note_by_id(id, patch_note)
+        if 'Authorization' in request.headers:
+            bearer = request.headers['Authorization']
+            if bearer.startswith('Bearer '):
+                token = bearer[7:]
 
-  return jsonify({"message": patched})
+        if not token:
+            return jsonify({'message': 'Token tidak ditemukan'}), 401
 
-if __name__ == '__main__':
-  app.run(debug=True, host="0.0.0.0", port=5000)
+        try:
+            data = jwt.decode(token, SECRET_KEY, algorithms=['HS256'])
+            current_user = {
+              "username" : data['username'],
+              "role" : data['role']
+            }
+        except jwt.ExpiredSignatureError:
+            return jsonify({'message': 'Token expired'}), 401
+        except jwt.InvalidTokenError:
+            return jsonify({'message': 'Token invalid'}), 401
+
+        return f(current_user, *args, **kwargs)
+    return decorated
+
+
+@app.route("/notes", methods=["POST"])
+@token_required
+def post_notes(current_user):
+    
+    data = request.get_json()
+    note = data.get('note')
+    insert_note(note, current_user)
+    
+    if not note:
+      return jsonify({"message": "Catatan tidak boleh kosong"})
+    else:
+      return jsonify({'message': f'Catatan {note} berhasil disimpan oleh user {current_user["username"]}'})
+
+
+@app.route("/notes", methods=["GET"])
+@token_required
+def get_notes(current_user):
+    notes_data = get_all_notes(current_user)
+    result = [
+        {"id": id, "note": note, "created_by": created_by, "created_at": created_at}
+        for (id, note, created_by, created_at) in notes_data
+    ]
+    return jsonify(result), 200
+
+
+@app.route("/notes/<int:id>", methods=["DELETE"])
+@token_required
+def del_notes(current_user, id):
+    success = delete_note_by_id(id, current_user)
+
+    if success:
+        return jsonify({"message": f"Catatan Dengan ID {id} Berhasil Dihapus"}), 200
+    else:
+        return jsonify({"message": f"Catatan Dengan ID {id} Tidak Ditemukan atau bukan milik Anda"}), 404
+
+
+@app.route("/notes/<int:id>", methods=["PATCH"])
+@token_required
+def patch_notes(current_user, id):
+    patch_note = request.get_json()
+
+    patched = patch_note_by_id(id, patch_note, current_user)
+
+    return jsonify({"message": patched})
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=5000)
